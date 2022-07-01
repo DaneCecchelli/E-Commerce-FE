ef63cc3bd4be6c7bb723a9c07b17b294bc78aa7d
DaneCecchelli <danececchelli@hotmail.com>
2022-07-01 18:14:21 -0400

    Adding further testing

---
+++ __tests__/CartCount.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import wait from 'waait';
+import CartCount from '../components/CartCount';
+
+describe('<CartCount/>', () => {
+  it('renders', () => {
+    render(<CartCount count={10} />);
+  });
+  it('Matches snapshot', () => {
+    const { container } = render(<CartCount count={11} />);
+    expect(container).toMatchSnapshot();
+  });
+  it('updates via props', async () => {
+    const { container, rerender } = render(<CartCount count={11} />);
+    expect(container.textContent).toBe('11');
+
+    rerender(<CartCount count="12" />);
+    expect(container.textContent).toBe('1211');
+    await wait(400);
+    expect(container.textContent).toBe('12');
+    expect(container).toMatchSnapshot();
+  });
+});
